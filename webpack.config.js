1dafddd1f6f8d8f6c2a81faeb79ed7404ad7a3e9
ch3ber <eber4202@hotmail.com>
2021-09-07 14:08:35 -0500

    se agregaron los plugins para fonts, minificazion de archivos y se agregaron las alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path'); //modulo de node para la resolucion de directorios
+const htmlWebpackPlugin = require('html-webpack-plugin'); //plugin para el manejo de html
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //plugin para el manejo de css
+const CopyPlugin = require('copy-webpack-plugin'); //plugin para copiar elementos
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin'); //plugin para minificar css
+const TerserPlugin = require('terser-webpack-plugin'); //minificar de una mejor forma
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-   entry: './src/index.js',
+   entry: './src/index.js', // archivo de entrada
    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'), //directorio de salida
+      filename: '[name].[contenthash].js',
+      assetModuleFilename: 'assets/images/[hash][ext][query]' //hashear el nombre de las imagenes
    },
    resolve: {
-      extensions: ['.js']
+      extensions: ['.js'], //manejo de las extensiones
+      alias: { //definir alias
+         '@utils': path.resolve(__dirname, 'src/utils/'),
+         '@templates': path.resolve(__dirname, 'src/templates/'),
+         '@styles': path.resolve(__dirname, 'src/styles/'),
+         '@images': path.resolve(__dirname, 'src/assets/images/'),
+      }
    },
    module: {
       rules: [
@@ -18,36 +28,57 @@ module.exports = {
             test: /\.m?js$/,
             exclude: /node_modules/,
             use: {
-               loader: 'babel-loader'
+               loader: 'babel-loader' //transpilador de javascript
             }
          },
          {
             test: /\.css|.styl$/i,
             use: [MiniCssExtractPlugin.loader,
-            'css-loader',
-            'stylus-loader'
+            'css-loader', //entender el codigo css
+            'stylus-loader' //entender el codigo de stylus
             ],
          },
          {
-            test: /\.png/,
-            type: 'asset/resource'
+            test: /\.png$/,
+            type: 'asset/resource' //mover imagenes con el modulo asset
+         },
+         {
+            test: /\.(woff|woff2)$/,
+            type: "asset/resource", //mover fuentes con el modulo asset
+            generator: {
+               filename: "assets/fonts/[name].[contenthash].[ext]"
+            }
          }
       ]
    },
    plugins: [
+      // configuracion del plugin html
       new htmlWebpackPlugin({
          inject: true,
          template: './public/index.html',
          filename: './index.html',
       }),
-      new MiniCssExtractPlugin(),
+      // configuracion del plugin para copiar y hashear el css
+      new MiniCssExtractPlugin({
+         filename: 'assets/[name].[contenthash].css'
+      }),
+      // configuracion para copiar elementos
       new CopyPlugin({
          patterns: [
             {
-               from: path.resolve(__dirname, "src", "assets/images"),
-               to: "assets/images"
+               from: path.resolve(__dirname, "src", "assets/images"), //directorio de inicio
+               to: "assets/images" //directorio final
             }
          ]
       }),
-   ]
+      new Dotenv(),
+   ],
+   //configuracion de optimizacion
+   optimization: {
+      minimize: true, //activar la minificacion
+      minimizer: [
+         new CssMinimizerPlugin(), //minificar css
+         new TerserPlugin(), //minificar mejor
+      ]
+   }
 }
